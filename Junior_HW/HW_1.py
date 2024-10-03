782bc25bb18c3f6666fe4bf13073ac1d624399de
Yechan Kim <yechankim512@gmail.com>
2024-10-04 00:03:27 +0900

    first commit

---
+++ Junior_HW/HW_1.py
@@ -0,0 +1,10 @@
+def generate_hello_world_str():
+    return "Hello World 김예찬"
+
+def print_console():
+    print(generate_hello_world_str())
+    
+if __name__ == "__main__":
+    print_console()
+    
+    
